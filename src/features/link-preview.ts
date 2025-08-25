7c86e2ff13ce49ac08766de764aa68133c10fd08
ImRayy <souradeep_roy@pm.me>
2025-08-26 00:10:45 +0530

    feat: add endpoint to fetch url metadata

---
+++ src/features/link-preview.ts
@@ -0,0 +1,63 @@
+import * as dns from "node:dns";
+import type { ContentfulStatusCode } from "hono/utils/http-status";
+import { getLinkPreview } from "link-preview-js";
+
+export type LinkPreview = {
+  url: string;
+  title: string;
+  siteName: string | undefined;
+  description: string | undefined;
+  mediaType: string;
+  contentType: string | undefined;
+  images: string[];
+  videos: {
+    url: string | undefined;
+    secureUrl: string | null | undefined;
+    type: string | null | undefined;
+    width: string | undefined;
+    height: string | undefined;
+  }[];
+  favicons: string[];
+  charset: string | null;
+};
+
+export interface LinkPreviewResponsse {
+  status: ContentfulStatusCode;
+  message: string;
+  data: Partial<LinkPreview> | null;
+}
+
+export const fetchLinkPreview = async (
+  websiteUrl: string,
+): Promise<LinkPreviewResponsse> => {
+  try {
+    const response = await getLinkPreview(websiteUrl, {
+      resolveDNSHost: async (url: string) => {
+        return new Promise((resolve, reject) => {
+          const hostname = new URL(url).hostname;
+          dns.lookup(hostname, (err, address) => {
+            if (err) {
+              reject(err);
+            }
+            resolve(address);
+          });
+        });
+      },
+    });
+
+    return {
+      status: 200,
+      message: "Successfully fetched link preview",
+      data: response,
+    };
+    // biome-ignore lint/suspicious/noExplicitAny: false
+  } catch (error: any) {
+    console.error(error);
+
+    return {
+      status: 502,
+      message: error.message || "Failed to fetch link preview",
+      data: null,
+    };
+  }
+};
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+import { fetchLinkPreview } from "./features/link-preview.js";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("link-preview-api");
+});
+
+app.post("/api/v1", async (c) => {
+  const { url } = await c.req.json();
+
+  if (!url || url.trim() === "") {
+    throw new HTTPException(400, { message: "url is required" });
+  }
+
+  const metadata = await fetchLinkPreview(url);
+
+  if (!metadata.data) {
+    throw new HTTPException(500, { message: "Failed to fetch link preview" });
+  }
+
+  return c.json(metadata);
+});
+
+export default app;
